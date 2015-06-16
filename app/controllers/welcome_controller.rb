9515c4421e60e90a08eae577a8f08cedc2440f4b
Steve Galbraith <steve@stevegalbraith.com>
2015-06-15 18:23:54 -0700

    Added search to home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-    @categories = Category.all
+    if params[:search]
+      @categories = Snippet.where("lower(title) ILIKE ? OR lower(block) ILIKE ?", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
+    else
+      @categories = Category.all
+    end
   end
-  
+
 end
