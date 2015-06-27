6ffba95fb48e436d1967c4b1d17de0c422d02d32
Steve Galbraith <steve@stevegalbraith.com>
2015-06-26 23:35:38 -0700

    AJAXified the voting in the snippet show page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,31 +1,42 @@
 class VotesController < ApplicationController
 
   def create
-    snippet = Snippet.find params[:snippet_id]
-    vote = current_user.votes.new(vote_params)
-    vote.snippet = snippet
-    if vote.save
-      redirect_to snippet
-    else
-      redirect_to snippet
+    @snippet = Snippet.find params[:snippet_id]
+    @vote = current_user.votes.new(vote_params)
+    @vote.snippet = @snippet
+    respond_to do |format|
+      if @vote.save
+        format.html { redirect_to @snippet }
+        format.js { render }
+      else
+        format.html { redirect_to @snippet }
+        format.js { render }
+      end
     end
   end
 
   def update
-    snippet = Snippet.find params[:snippet_id]
-    vote = current_user.votes.find params[:id]
-    if vote.update(vote_params)
-      redirect_to snippet
-    else
-      redirect_to snippet
+    @snippet = Snippet.find params[:snippet_id]
+    @vote = current_user.votes.find params[:id]
+    respond_to do |format|
+      if @vote.update(vote_params)
+        format.html { redirect_to @snippet }
+        format.js { render :update }
+      else
+        format.html { redirect_to @snippet }
+        format.js { render :update }
+      end
     end
   end
 
   def destroy
-    snippet = Snippet.find params[:snippet_id]
-    vote = current_user.votes.find params[:id]
-    vote.destroy
-    redirect_to snippet
+    @snippet = Snippet.find params[:snippet_id]
+    @vote = current_user.votes.find params[:id]
+    @vote.destroy
+    respond_to do |format|
+      format.html { redirect_to @snippet }
+      format.js { render :destroy }
+    end
   end
 
   private
