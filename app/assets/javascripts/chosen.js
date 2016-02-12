a4d01b4ee53046531edb9c5d46f37c5979e8a6af
Steve Galbraith <steve@stevegalbraith.com>
2016-02-12 09:45:29 -0700

    Removes help text for new snippet and updates placeholder text as hint

--- app/assets/javascripts/chosen.js
+++ app/assets/javascripts/chosen.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $(".chosen-select").chosen({
     no_results_text: "No matching manufacturers found",
-    placeholder_text_multiple: "Select manufacturers for this coupon. If coupon is for all manufacturers, leave empty.",
+    placeholder_text_multiple: "Select the most relevent language first, then add others it could apply to as well.",
     width: "100%"
     });
 
