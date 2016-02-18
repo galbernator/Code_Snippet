2f0732b997514f5c5c80c482e5a5925e50fd152d
Steve Galbraith <steve@stevegalbraith.com>
2016-02-17 23:06:35 -0700

    Adds admin namespave and categories controller as well as user roles and admin authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def authorize_admin
+    redirect_to root_path, notice: 'You do not have permission to view that page,' unless user_signed_in_as_admin?
+  end
+
+  def user_signed_in_as_admin
+    current_user.present? && current_user.admin?
+  end
+
   def authorize
     redirect_to new_session_path, notice: "Please sign in" unless user_signed_in?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212061326) do
+ActiveRecord::Schema.define(version: 20160218053600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160212061326) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
